3ed12a96fd1083f46da6f94748e9c50cdb0e23c7
julian <julian.cadenas@gmail.com>
2017-08-08 20:20:06 -0400

    Build frontend skeleton

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <header>
+      <h2>Hi-dilly ho-dilly, neighbor-inho!</h2>
+    </header>
+  );
+};
+
+export default App;
---
+++ frontend/index.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import configureStore from './store/store';
+import Root from './components/root';
+
+// TESTING
+// import * as APIUtil from './util/api_util';
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  let store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
+
+  // TESTING
+  window.store = store;
+  }
+);
