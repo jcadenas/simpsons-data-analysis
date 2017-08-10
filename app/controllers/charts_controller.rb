185762f8e1bfe92e760d423f03b1d4ce2f4e3950
julian <julian.cadenas@gmail.com>
2017-08-09 20:15:33 -0400

    Create top character json response

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,12 +1,26 @@
+require 'json'
+
 class ChartsController < ApplicationController
 
   def top_characters
-    @top_characters = ScriptLines
-      .select()
 
+    @top_characters = ActiveRecord::Base.connection.execute(<<-SQL)
+    SELECT
+      script_lines.character_id, characters.normalized_name, COUNT(script_lines.id) as line_count
+    FROM
+      script_lines
+    JOIN
+      characters ON script_lines.character_id = characters.character_id
+    GROUP BY
+      script_lines.character_id, characters.normalized_name
+    ORDER BY
+      COUNT(script_lines.id) desc
+    LIMIT 20
+    SQL
 
+    @top_characters = JSON.parse(@top_characters.to_json)
+    render :top_characters
 
-    render json: ['oh shit, top_characters worked']
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  get '/api/charts/top_characters', to: 'charts#top_characters'
+  get '/api/charts/top_characters', to: 'charts#top_characters', defaults: { format: :json }
 
 end
