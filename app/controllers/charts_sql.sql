019cfdca08f33b7d7f9d0d96f367ebc83c12fcb1
julian <julian.cadenas@gmail.com>
2017-08-11 20:49:39 -0400

    Update SQL Code for min/max

--- app/controllers/charts_sql.sql
+++ app/controllers/charts_sql.sql
@@ -10,3 +10,16 @@ GROUP BY
 ORDER BY
   COUNT(script_lines.id) desc
 LIMIT 30;
+
+-- Top characters by spoken line count | Min/Max
+SELECT
+  script_lines.character_id, characters.normalized_name, COUNT(script_lines.id) as line_count
+FROM
+  script_lines
+JOIN
+  characters ON script_lines.character_id = characters.character_id
+GROUP BY
+  script_lines.character_id, characters.normalized_name
+ORDER BY
+  COUNT(script_lines.id) desc
+LIMIT 30;
