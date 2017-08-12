c547d1b0a287a06fe1cd385b35c1232c7427fc99
julian <julian.cadenas@gmail.com>
2017-08-11 21:44:01 -0400

    Sucessfully run d3 with test data and true d3 library

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import TopCharacters from './top_characters';
 
 
 const App = () => {
   return (
     <header>
       <h2>Hi-dilly ho-dilly, neighbor-inho!</h2>
+      <TopCharacters data={[5,10,1,3, 7, 0, 2]} size={[500,200]} />
     </header>
   );
 };
