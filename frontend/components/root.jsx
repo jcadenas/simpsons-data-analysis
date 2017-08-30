4d3abb22e36e4b5e0a445249872695eb1274ef56
julian <julian.cadenas@gmail.com>
2017-08-29 22:21:46 -0400

    Change HashRouter to BrowserRouter

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 
 
 const Root = ({store}) => {
   return (
     <Provider store={store} >
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 };
